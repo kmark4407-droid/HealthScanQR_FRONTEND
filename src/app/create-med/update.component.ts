3d8b030654437bfebfe90168830cf0a2750333e0
Mark Joheun Kim <kmark4407@gmail.com>
2025-11-14 19:21:39 +0800

    Update update.component.ts

--- src/app/create-med/update.component.ts
+++ src/app/create-med/update.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { environment } from '../Environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-update-info',
