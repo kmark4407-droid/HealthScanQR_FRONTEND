e5a25c6ec1f8b2f7570c3bc634f3c9756163d934
Mark Joheun Kim <kmark4407@gmail.com>
2025-11-18 01:48:41 +0800

    Update auth.service.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap, catchError, throwError } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../Environments/environment';
 
 @Injectable({
   providedIn: 'root'
