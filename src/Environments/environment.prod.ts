9d92e99c44a0f798979ff002a9f77169b0426abc
Mark Joheun Kim <kmark4407@gmail.com>
2025-11-18 20:45:19 +0800

    Update environment.prod.ts

--- src/Environments/environment.prod.ts
+++ src/Environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://healthscanqr-backend.onrender.com'  // ← ADD /api here
+  apiUrl: 'https://healthscanqr-backend.onrender.com/api'  // ← ADD /api here
 };
